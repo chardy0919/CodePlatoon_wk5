e8a9ced5065c15648b838e0cda169c8ea06ba9df
Cody Hardy <chardy0919@gmail.com>
2023-11-03 17:31:00 -0400

    last minute change

--- Assignments/react-hangman/hang_man/src/components/Hangman.jsx
+++ Assignments/react-hangman/hang_man/src/components/Hangman.jsx
@@ -7,7 +7,7 @@ function Hangman({wrongCount, currentWord}) {
         <p  class="max-w-screen-xl mx-auto p-8 text-right">
         +---+<br />
         |&emsp;&emsp;|<br />
-        {wrongCount >= 1 ? "😵" : ""}&emsp;&emsp;|<br />
+        {wrongCount >= 1 ? "💀" : ""}&emsp;&emsp;|<br />
         {wrongCount >= 4 ? "/" : ""}{wrongCount >= 2 ? "|" : ""}{wrongCount >= 3 ? "\\" : ""}&emsp;&emsp;|<br />
         {wrongCount >= 6 ? "/" : ""}{wrongCount >= 5 ? "\\" : ""}&emsp;&emsp;|<br />
             |<br />
